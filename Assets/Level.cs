b6c0006b7c61d689739cda3bf0f37a5a1850803f
anandcr <anandcr635@gmail.com>
2020-07-23 13:12:19 +0530

    Initial Commit

---
+++ Assets/Level.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    int currentSceneIndex = 0; 
+
+    private void Start()
+    {   
+        StartCoroutine(LoadStartScene());
+    }
+
+
+    IEnumerator LoadStartScene()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(1);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
